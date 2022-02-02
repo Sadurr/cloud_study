80659c8adf003ad9a1baf4bd4e50e1579610fcf1
Filip Sadurski <75565576+Sadurr@users.noreply.github.com>
2022-02-02 19:02:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
+from flask import Flask
 import mysql.connector
 
-db = mysql.connector.connect(
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    db = mysql.connector.connect(
     host="sadurskidatabase.mysql.database.azure.com",
     user="filip_sadurski",
     passwd="AzurePa$$w0rd",
-    database="sadurskidatabase"
-)
+    database="sadurskidatabase")
 
-cursor=db.cursor()
+    cursor=db.cursor()
 
-cursor.execute("CREATE TABLE Names (name VARCHAR(50))")
-cursor.execute("INSERT INTO Names (name) VALUES ('Filip')")
+    cursor.execute("CREATE TABLE Names (name VARCHAR(50))")
+    cursor.execute("INSERT INTO Names (name) VALUES ('Filip')")
 
-cursor.execute("SELECT * FROM Names")
+    cursor.execute("SELECT * FROM Names")
 
-result=cursor.fetchall()
+    result=cursor.fetchall()
 
-for row in result:
-    print("Hello " + row)
+    for row in result:
+        print("Hello " + row)
+
+if __name__ == '__main__':
+    app.run()
+    
